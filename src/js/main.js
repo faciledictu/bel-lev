1e0e50311621f924cb1a55f0f5a54d33840f21bf
Dmitry Zhigulin <104712148+faciledictu@users.noreply.github.com>
2023-10-17 16:58:05 +0400

    Update sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,52 @@
-// Import our custom CSS
-// import "../scss/styles.scss";
-import "swiper/css";
+import Swiper from 'swiper';
+import { Thumbs } from 'swiper/modules';
 
-import Swiper from "swiper";
-import { Thumbs } from "swiper/modules";
+//Swiper
+const swipers = {};
 
-// Import only the Bootstrap components we need
+const carousels = document.querySelectorAll('[data-swiper-type="carousel"]');
 
-const makeStickyOnBottomEdge = (elem) => {
-  elem.style.position = "sticky";
-  // elem.style.top = `calc(var(--vh, 1vh) * 100 - ${elem.offsetHeight}px)`;
-  elem.style.top = `calc(100vh - ${elem.offsetHeight}px)`;
-  console.log(elem.offsetHeight);
-};
-
-const stickyCards = document.querySelectorAll('[data-sticky="on-bottom"]');
-
-const makeStickyCards = () => {
-  // let vh = window.innerHeight * 0.01;
-  // document.documentElement.style.setProperty("--vh", `${vh}px`);
-  stickyCards.forEach(makeStickyOnBottomEdge);
-};
-
-const swiper = {};
-
-["revolutsii", "gorkogo", "komsomolskaya", "belinskogo"].forEach((id) => {
-  swiper[id] = {};
-
-  swiper[id].thumbs = new Swiper(`#swiper-${id}-thumbs`, {
+carousels.forEach((e) => {
+  const swiperParams = {
     spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
+  };
 
-  swiper[id].swiper = new Swiper(`#swiper-${id}`, {
-    spaceBetween: 10,
-    modules: [Thumbs],
-    thumbs: {
-      swiper: swiper[id].thumbs,
-    },
-  });
+  const thumbCarusel = document.querySelector(
+    `[data-swiper-thumbs-for="${e.id}"]`
+  );
+
+  if (thumbCarusel) {
+    const thumbsSwiper = new Swiper(thumbCarusel, {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+
+    swiperParams.modules = [Thumbs];
+    swiperParams.thumbs = { swiper: thumbsSwiper };
+  }
+
+  swipers[e.id] = new Swiper(e, swiperParams);
+  console.log(swipers);
 });
 
 const resizeObserver = new ResizeObserver((entries) => {
   for (const entry of entries) {
     makeStickyOnBottomEdge(entry.target);
   }
-
-  console.log("Size changed");
 });
+
+//Sticky Cards
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty("--vh", `${vh}px`);
+// elem.style.top = `calc(var(--vh, 1vh) * 100 - ${elem.offsetHeight}px)`;
+
+const stickyCards = document.querySelectorAll('[data-sticky="on-bottom"]');
+
+const makeStickyOnBottomEdge = (elem) => {
+  elem.style.position = 'sticky';
+  elem.style.top = `calc(100vh - ${elem.offsetHeight}px)`;
+};
 
 stickyCards.forEach((elem) => resizeObserver.observe(elem));
