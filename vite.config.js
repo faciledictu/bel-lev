bb6f07dcb5b50e4f59754dabde7da50b399a82ca
Dmitry Zhigulin <104712148+faciledictu@users.noreply.github.com>
2024-02-15 00:12:05 +0400

    Remove hash from deploy filenames

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
   server: {
-    port: 8080
-  }
-}
+    port: 8080,
+  },
+};
